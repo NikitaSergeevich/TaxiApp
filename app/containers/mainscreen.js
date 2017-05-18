40c5bf8feff5aa426374ad7056228db2ad20df0c
Woody\Woody-Company <nikita0015nikita@gmail.com>
2017-05-18 18:36:51 +0300

    experiments with map (marker rotation, map animation), new key in manifest

--- app/containers/mainscreen.js
+++ app/containers/mainscreen.js
@@ -2,22 +2,73 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StatusBar
+    StatusBar,
+    Animated,
+    Easing
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 import { videoModuleStyles } from '../resources/styles';
 
 export default class MainScreen extends Component {
 
-    render() {
-        return (
-            <View style={{
-                flex: 1,
-                backgroundColor: 'red'
-            }}>
-                <StatusBar hidden={true} />
+    constructor(props) {
+        super(props);
 
-            </View>
+        this.state = {
+            title: '',
+            expanded: false,
+            animation: new Animated.Value(0),
+            minHeight: 2,
+            maxHeight: 0,
+            spinValue: new Animated.Value(0)
+        };
+    }
+
+    componentDidMount() {
+        this.toggle();
+    }
+
+    toggle() {
+        /*Animated.timing(
+            this.state.animation,
+            {
+                duration: 3000,
+                toValue: 20,
+            }
+        ).start();*/
+
+        Animated.timing(
+            this.state.spinValue,
+            {
+                toValue: 1,
+                duration: 3000,
+                easing: Easing.linear
+            }
+        ).start()
+    }
+
+    render() {
+        const spin = this.state.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+        })
+        return (
+            <Animated.View style={{ position: 'absolute', top: this.state.animation, bottom: this.state.animation, left: 0, right: 0 }}>
+                <MapView
+                    style={{
+                        flex: 1
+                    }}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                    <MapView.Marker.Animated style={{ backgroundColor: '#ccc', transform: [{ rotate: spin }], }} coordinate={{ latitude: 37.78825, longitude: -122.4324 }} image={require('../resources/icons/image.png')} />
+                </MapView>
+            </Animated.View>
         );
     }
 }
