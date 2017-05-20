1b7f468af59811a8091c4cca7268b3deb03c4d51
GaryGolf <garygolf@mail.ru>
2017-05-20 05:35:46 +0300

    add google map to android

---
+++ src/containers/map.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+const MapView = require('react-native-maps');
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+interface Region {
+    latitude: number
+    longitude: number
+    latitudeDelta: number
+    longitudeDelta: number
+}
+interface Props {
+    provider?: any
+}
+interface State {
+    region: Region
+}
+
+export default class StaticMap extends React.Component <Props, State> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+    };
+  }
+
+   render() {
+    return (
+      <View style={styles.container}>
+          <MapView
+            provider={this.props.provider}
+            style={styles.map}
+            scrollEnabled={false}
+            zoomEnabled={false}
+            pitchEnabled={false}
+            rotateEnabled={false}
+            initialRegion={this.state.region}
+          >
+            <MapView.Marker
+                title="This is a title"
+                description="This is a description"
+                coordinate={this.state.region}
+            />
+          </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+  },
+  
+  scrollview: {
+    alignItems: 'center',
+    paddingVertical: 40,
+  },
+  map: {
+    width: 250,
+    height: 250,
+  },
+});
