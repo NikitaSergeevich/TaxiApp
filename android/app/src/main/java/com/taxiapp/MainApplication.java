d530614ad998b6d3d8d8ec92b55c7a09638f73ac
NikitaSergeevich <dercoebo@mail.ru>
2017-06-18 11:09:09 +0300

    react-native-intractable for gestures (linking), gradle version upgrade

--- android/app/src/main/java/com/taxiapp/MainApplication.java
+++ android/app/src/main/java/com/taxiapp/MainApplication.java
@@ -7,6 +7,7 @@ import com.microsoft.codepush.react.CodePush;
 import com.horcrux.svg.SvgPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.wix.interactable.Interactable;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new Interactable(),
                     new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
                     new SvgPackage(),
                     new OrientationPackage(),
